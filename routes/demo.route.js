f89f71843d4c502b81300acb6ff38ddea91c1388
pichead <61868523+pichead@users.noreply.github.com>
2024-02-19 02:08:29 +0700

    add prisma client

--- routes/demo.route.js
+++ routes/demo.route.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 const { SuccessResponseObject } = require("../common/http");
 
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 const r = Router();
 
 r.get("/", (req, res) => {
