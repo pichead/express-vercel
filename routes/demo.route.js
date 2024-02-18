550d2cfe92e7fb87c82dfaa2d33c328987ca13b0
pichead <61868523+pichead@users.noreply.github.com>
2024-02-19 02:22:18 +0700

    fix prisma

--- routes/demo.route.js
+++ routes/demo.route.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { SuccessResponseObject } = require("../common/http");
 
-const { PrismaClient } = require('../prisma/generated/client')
+const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
